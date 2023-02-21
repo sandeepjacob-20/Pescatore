b84b9f6cbe661fb495ef42b3422ff763d7fdc89b
Sandeep Jacob <sandeepjacob1@gmail.com>
2023-02-21 13:41:25 +0530

    fixed bugs

--- pescatore_api.py
+++ pescatore_api.py
@@ -2,20 +2,12 @@
 from fastapi import FastAPI
 import detection as detect
 from urllib.parse import unquote
-import training as train
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return "Welcome to Pescatore API. Pescatore API can be used to detect phishing websites by passing in the URL. Use ../verify?url=[enter your URL here] to check for phishing. For further details please visit ../docs. Follow the project on github : https://github.com/sandeepjacob-20/Pescatore.git . Thank You."
-
-@app.get("/train")
-def root(key: str):
-    if key == "lospolloshermanos":
-        return train.train_model()
-    else:
-        return {"Error":"Password incorrect"}
 
 @app.get("/verify")
 def home(url: str):
