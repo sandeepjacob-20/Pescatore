c08aa00df72b8ef9c186d5466416baa24610478f
Sandeep Jacob <sandeepjacob1@gmail.com>
2023-02-21 23:55:33 +0530

    temporarily disabled site info

--- detection.py
+++ detection.py
@@ -3,7 +3,6 @@ import feature_extraction as fe
 import whois
 from urllib.parse import urlparse
 import pycountry
-import csv
 import pandas as pd
 
 #To check if the domain name already exists in the training dataset
@@ -30,29 +29,36 @@ def check(url):
     result = loaded_model.predict(features)
 
     #extracts the details of the url
-    site_data = whois.whois(urlparse(url).netloc)
-
     try:
-        domain_name = site_data.domain_name
-        if(type(domain_name) is list ):
-            domain_name=domain_name[1].lower()
-        if domain_name.isupper():
-            domain_name=domain_name.lower()
-        doesExist = databaseCheck(domain_name) #checks if the domain name already exists in the training dataset.
+        site_data = whois.whois(urlparse(url).netloc)
+
+        try:
+            domain_name = site_data.domain_name
+            if(type(domain_name) is list ):
+                domain_name=domain_name[1].lower()
+            if domain_name.isupper():
+                domain_name=domain_name.lower()
+            doesExist = databaseCheck(domain_name) #checks if the domain name already exists in the training dataset.
+        except:
+            domain_name="none"
+            doesExist=1
+
+        registrar = site_data.registrar
+
+        creation_date = site_data.creation_date
+        if (type(creation_date) is list):
+            creation_date=creation_date[0]
+
+        try:
+            country_name = pycountry.countries.get(alpha_2=site_data.country).name
+        except:
+            country_name = "unavailable"
     except:
         domain_name="none"
-        doesExist=1
-
-    registrar = site_data.registrar
-
-    creation_date = site_data.creation_date
-    if (type(creation_date) is list):
-        creation_date=creation_date[0]
-
-    try:
-        country_name = pycountry.countries.get(alpha_2=site_data.country).name
-    except:
+        registrar="none"
+        creation_date="none"
         country_name = "unavailable"
+        doesExist=1
 
     if result==0:
         if doesExist == 0:
